692df18cbdcbc0ea5a01fc5ede93806682e0ae1d
Kerby Martino <kerbymart@gmail.com>
2015-01-05 19:29:37 +0800

    apply configuration from arquillian-gae-embedded see: https://github.com/alesj/arquillian-container-gae

---
+++ dreamcode-gae/src/test/java/com/textquo/dreamcode/AppEngineEmbeddedClientTestCase.java
@@ -0,0 +1,105 @@
+/*
+ * JBoss, Home of Professional Open Source.
+ * Copyright 2010, Red Hat Middleware LLC, and individual contributors
+ * as indicated by the @author tags. See the copyright.txt file in the
+ * distribution for a full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+/**
+ *
+ * Copyright (c) 2014 Kerby Martino and others. All rights reserved.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * 	   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ *     __                                         __
+ * .--|  .----.-----.---.-.--------.----.-----.--|  .-----.
+ * |  _  |   _|  -__|  _  |        |  __|  _  |  _  |  -__|
+ * |_____|__| |_____|___._|__|__|__|____|_____|_____|_____|
+ *
+ */
+package com.textquo.dreamcode;
+
+import junit.framework.Assert;
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.container.test.api.OperateOnDeployment;
+import org.jboss.arquillian.container.test.api.RunAsClient;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import java.net.URL;
+
+/**
+ * @author <a href="mailto:ales.justin@jboss.org">Ales Justin</a>
+ */
+@RunWith(Arquillian.class)
+@RunAsClient
+public class AppEngineEmbeddedClientTestCase {
+    /**
+     * Deployment for the test
+     *
+     * @return web archive
+     */
+    @Deployment(name = "default")
+    public static WebArchive getTestArchive() {
+        return ShrinkWrap.create(WebArchive.class, "simple.war")
+                .addClass(TestServlet.class)
+                .setWebXML("gae-web.xml")
+                .addAsWebInfResource("appengine-web.xml")
+                .addAsWebInfResource("logging.properties");
+    }
+
+    @Test
+    @OperateOnDeployment("default")
+    public void shouldBeAbleToInvokeServletInDeployedWebApp() throws Exception {
+        String body = readAllAndClose(new URL("http://localhost:8080/test").openStream());
+
+        Assert.assertEquals(
+                "Verify that the servlet was deployed and returns expected result",
+                TestServlet.MESSAGE,
+                body);
+    }
+
+    private String readAllAndClose(InputStream is) throws Exception {
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        try {
+            int read;
+            while ((read = is.read()) != -1) {
+                out.write(read);
+            }
+        } finally {
+            try {
+                is.close();
+            } catch (Exception ignored) {
+            }
+        }
+        return out.toString();
+    }
+}
